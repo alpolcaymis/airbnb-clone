eee8fb9b585fddde894ba01cc71bb9b45fbad4d6
alpolcaymis <alpmis99@gmail.com>
2024-02-17 15:40:57 +0300

    little bug fixed

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -9,6 +9,8 @@ declare global {
   var cloudinary: any;
 }
 
+const uploadPreset = "kogqhiby";
+
 interface ImageUploadProps {
   onChange: (value: string) => void;
   value: string;
@@ -25,39 +27,44 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ onChange, value }) => {
   return (
     <CldUploadWidget
       onUpload={handleUpload}
-      uploadPreset="kogqhiby"
-      options={{ maxFiles: 1 }}
+      uploadPreset={uploadPreset}
+      options={{
+        maxFiles: 1,
+      }}
     >
       {({ open }) => {
         return (
           <div
             onClick={() => open?.()}
             className="
-        relative
-        cursor-pointer
-        hover:opacity-70
-        transition
-        border-dashed
-        border-2
-        p-20
-        border-neutral-300
-        flex
-        flex-col
-        justify-center
-        items-center
-        gap-4
-        text-neutral-600
-        "
+              relative
+              cursor-pointer
+              hover:opacity-70
+              transition
+              border-dashed 
+              border-2 
+              p-20 
+              border-neutral-300
+              flex
+              flex-col
+              justify-center
+              items-center
+              gap-4
+              text-neutral-600
+            "
           >
-            <TbPhotoPlus />
+            <TbPhotoPlus size={50} />
             <div className="font-semibold text-lg">Click to upload</div>
             {value && (
-              <div className="absolute inset-0 w-full h-full">
+              <div
+                className="
+              absolute inset-0 w-full h-full"
+              >
                 <Image
-                  alt="Upload"
                   fill
                   style={{ objectFit: "cover" }}
                   src={value}
+                  alt="House"
                 />
               </div>
             )}
