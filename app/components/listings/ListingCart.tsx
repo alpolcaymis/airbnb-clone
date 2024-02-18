d11df29fe48c247c546fe44c4aad858ed9651e0f
alpolcaymis <alpmis99@gmail.com>
2024-02-18 14:41:16 +0300

    18 şubat

---
+++ app/components/listings/ListingCart.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import useCountries from "@/app/hooks/useCountries";
+import { SafeUser } from "@/app/types";
+import { Listing, Reservation } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { useCallback, useMemo } from "react";
+
+interface ListingCartProps {
+  data: Listing;
+  reservation?: Reservation;
+  onAction?: (id: string) => void;
+  disabled?: boolean;
+  actionLabel?: string;
+  actionId?: string;
+  currentUser?: SafeUser | null;
+}
+
+const ListingCart: React.FC<ListingCartProps> = ({
+  data,
+  reservation,
+  onAction,
+  disabled,
+  actionLabel,
+  actionId = "",
+  currentUser,
+}) => {
+  const router = useRouter();
+  const { getByValue } = useCountries();
+
+  const location = getByValue(data.locationValue);
+
+  const handleCancel = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
+
+      if (disabled) {
+        return;
+      }
+      onAction?.(actionId);
+    },
+    [onAction, actionId, disabled]
+  );
+
+  const price = useMemo(() => {
+    if (reservation) {
+      return reservation.totalPrice;
+    }
+    return data.price;
+  }, [reservation, data.price]);
+
+  return <div>ListingCart</div>;
+};
+
+export default ListingCart;
