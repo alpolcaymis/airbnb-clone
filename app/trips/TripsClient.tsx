552036ee0e2183fc8cad5282c046fc744ce20232
alpolcaymis <alpmis99@gmail.com>
2024-02-23 15:00:11 +0300

    imports order arranged

--- app/trips/TripsClient.tsx
+++ app/trips/TripsClient.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import Container from "../components/Container";
-import Heading from "../components/Heading";
-import { SafeReservation, SafeUser } from "../types";
-import { useCallback, useState } from "react";
+import { toast } from "react-hot-toast";
 import axios from "axios";
-import toast from "react-hot-toast";
-import { error } from "console";
-import ListingCart from "../components/listings/ListingCard";
+import { useCallback, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { SafeReservation, SafeUser } from "@/app/types";
+
+import Heading from "@/app/components/Heading";
+import Container from "@/app/components/Container";
+import ListingCard from "@/app/components/listings/ListingCard";
 
 interface TripsClientProps {
   reservations: SafeReservation[];
@@ -48,16 +49,28 @@ const TripsClient: React.FC<TripsClientProps> = ({
         title="Trips"
         subtitle="Where you've been and where you're going"
       />
-      <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-flow-col-6 gap-8">
-        {reservations.map((reservation) => (
-          <ListingCart
+      <div
+        className="
+          mt-10
+          grid 
+          grid-cols-1 
+          sm:grid-cols-2 
+          md:grid-cols-3 
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+      >
+        {reservations.map((reservation: any) => (
+          <ListingCard
             key={reservation.id}
             data={reservation.listing}
             reservation={reservation}
             actionId={reservation.id}
             onAction={onCancel}
             disabled={deletingId === reservation.id}
-            actionLabel="Cancel Reservation"
+            actionLabel="Cancel reservation"
             currentUser={currentUser}
           />
         ))}
