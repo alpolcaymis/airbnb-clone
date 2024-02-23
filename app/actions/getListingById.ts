0bd2f9ceb5b9125c1d7820e704a980375dcdf25a
alpolcaymis <alpmis99@gmail.com>
2024-02-23 14:36:26 +0300

    IsoString -> toString()

--- app/actions/getListingById.ts
+++ app/actions/getListingById.ts
@@ -7,6 +7,7 @@ interface IParams {
 export default async function getListingById(params: IParams) {
   try {
     const { listingId } = params;
+
     const listing = await prisma.listing.findUnique({
       where: {
         id: listingId,
@@ -22,12 +23,12 @@ export default async function getListingById(params: IParams) {
 
     return {
       ...listing,
-      createdAt: listing.createdAt.toISOString(),
+      createdAt: listing.createdAt.toString(),
       user: {
         ...listing.user,
-        createdAt: listing.user.createdAt.toISOString(),
-        updatedAt: listing.user.updatedAt.toISOString(),
-        emailVerified: listing.user.emailVerified?.toISOString() || null,
+        createdAt: listing.user.createdAt.toString(),
+        updatedAt: listing.user.updatedAt.toString(),
+        emailVerified: listing.user.emailVerified?.toString() || null,
       },
     };
   } catch (error: any) {
