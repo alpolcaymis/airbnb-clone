cd8ac757d52c7f8dfebc149a929eefc7bdf1857e
alpolcaymis <alpmis99@gmail.com>
2024-02-23 14:55:21 +0300

    onCancel -> onDelete

--- app/properties/PropertiesClient.tsx
+++ app/properties/PropertiesClient.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import Container from "../components/Container";
-import Heading from "../components/Heading";
-import { SafeListing, SafeUser } from "../types";
-import { useCallback, useState } from "react";
+import { toast } from "react-hot-toast";
 import axios from "axios";
-import toast from "react-hot-toast";
-import { error } from "console";
-import ListingCart from "../components/listings/ListingCard";
+import { useCallback, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { SafeListing, SafeUser } from "@/app/types";
+
+import Heading from "@/app/components/Heading";
+import Container from "@/app/components/Container";
+import ListingCard from "@/app/components/listings/ListingCard";
 
 interface PropertiesClientProps {
   listings: SafeListing[];
@@ -22,7 +23,7 @@ const PropertiesClient: React.FC<PropertiesClientProps> = ({
   const router = useRouter();
   const [deletingId, setDeletingId] = useState("");
 
-  const onCancel = useCallback(
+  const onDelete = useCallback(
     (id: string) => {
       setDeletingId(id);
 
@@ -45,13 +46,25 @@ const PropertiesClient: React.FC<PropertiesClientProps> = ({
   return (
     <Container>
       <Heading title="Properties" subtitle="List of your properties" />
-      <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-flow-col-6 gap-8">
-        {listings.map((listing) => (
-          <ListingCart
+      <div
+        className="
+          mt-10
+          grid 
+          grid-cols-1 
+          sm:grid-cols-2 
+          md:grid-cols-3 
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+      >
+        {listings.map((listing: any) => (
+          <ListingCard
             key={listing.id}
             data={listing}
             actionId={listing.id}
-            onAction={onCancel}
+            onAction={onDelete}
             disabled={deletingId === listing.id}
             actionLabel="Delete property"
             currentUser={currentUser}
